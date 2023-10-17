bff520888b573eb20d6ad1c328603b8c51bb31a9
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-17 22:27:56 +0300

    squash!

--- Target/Function/Integration.d.ts
+++ Target/Function/Integration.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="node" />
 /**
  * @module Integration
  *
@@ -8,7 +7,6 @@ export default _default;
 import type Option from "../Interface/Option.js";
 import type { AstroIntegration } from "astro";
 export declare const Default: Omit<{} & {
-    Rome: any;
     Cache: {
         Search: string;
         Folder: string;
@@ -21,10 +19,11 @@ export declare const Default: Omit<{} & {
         Accomplished: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
         Fulfilled: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
         Read: ({ Input }: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-        Wrote: ({ Buffer }: import("files-pipe/Target/Interface/File.js").default) => Promise<Buffer>;
+        Wrote: ({ Buffer }: import("files-pipe/Target/Interface/File.js").default) => Promise<import("files-pipe/Target/Type/Buffer.js").Type>;
         Passed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<true>;
         Changed: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<import("files-pipe/Target/Interface/Plan.js").default>;
     }, "__proto__">;
     Logger: 2;
+    Rome: any;
 }, "__proto__">;
 export declare const Merge: import("typescript-esbuild/Target/Interface/Merge.js").default<import("typescript-esbuild/Target/Interface/Merge.js").Generic>;
--- Target/Variable/Option.d.ts
+++ Target/Variable/Option.d.ts
@@ -1,10 +1,8 @@
-/// <reference types="node" />
 /**
  * @module Option
  *
  */
 declare const _default: Omit<{} & {
-    Rome: any;
     Cache: {
         Search: string;
         Folder: string;
@@ -17,10 +15,11 @@ declare const _default: Omit<{} & {
         Accomplished: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
         Fulfilled: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
         Read: ({ Input }: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-        Wrote: ({ Buffer }: import("files-pipe/Target/Interface/File.js").default) => Promise<Buffer>;
+        Wrote: ({ Buffer }: import("files-pipe/Target/Interface/File.js").default) => Promise<import("files-pipe/Target/Type/Buffer.js").Type>;
         Passed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<true>;
         Changed: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<import("files-pipe/Target/Interface/Plan.js").default>;
     }, "__proto__">;
     Logger: 2;
+    Rome: any;
 }, "__proto__">;
 export default _default;
