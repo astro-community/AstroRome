316bf78dbeb24793e9884321032c9de452a57b89
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-21 23:55:24 +0300

    squash!

--- Source/Variable/Rome.ts
+++ Source/Variable/Rome.ts
@@ -3,7 +3,7 @@
  *
  */
 export default JSON.parse(
-	await (await import("../Function/Config.js")).default("rome.json")
+	await (await import("../Function/Configuration.js")).default("rome.json")
 ) satisfies Type;
 
 import type Type from "../Interface/Rome.js";
