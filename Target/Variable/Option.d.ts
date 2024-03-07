988e9ccb565fd785258e16891efff41c0fb9c02a
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-03-07 02:50:03 +0200

    squash!

--- Target/Variable/Option.d.ts
+++ Target/Variable/Option.d.ts
@@ -9,10 +9,10 @@ declare const _default: Omit<{} & {
         Failed: ({ Input }: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
         Accomplished: ({ Input, Output }: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
         Fulfilled: ({ Files }: import("files-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
-        Read: ({ Input }: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-        Wrote: ({ Buffer }: import("files-pipe/Target/Interface/File.js").default) => Promise<import("files-pipe/Target/Type/Buffer.js").Type>;
-        Passed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<true>;
-        Changed: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<import("files-pipe/Target/Interface/Plan.js").default>;
+        Read: ({ Input }: any) => Promise<string>;
+        Wrote: ({ Buffer }: any) => Promise<any>;
+        Passed: (On: any) => Promise<true>;
+        Changed: (Plan: any) => Promise<any>;
     }, "__proto__">;
     Cache: {
         Search: string;
